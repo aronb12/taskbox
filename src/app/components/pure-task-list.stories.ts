5140f488fed47e422feda4092ae8abec3bfaf5dd
Aron Bachmann Árnason <aronba@islandsbanki.is>
2021-07-05 13:31:24 +0000

    attempt #2 to force chromatic to build

--- src/app/components/pure-task-list.stories.ts
+++ src/app/components/pure-task-list.stories.ts
@@ -32,12 +32,12 @@ const Template: Story<PureTaskListComponent> = args => ({
 export const Default = Template.bind({});
 Default.args = {
   tasks: [
-    { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
-    { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
-    { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
-    { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
-    { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
-    { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+    { ...TaskStories.Default.args.task, id: '1', title: 'Task One' },
+    { ...TaskStories.Default.args.task, id: '2', title: 'Task Two' },
+    { ...TaskStories.Default.args.task, id: '3', title: 'Task Three' },
+    { ...TaskStories.Default.args.task, id: '4', title: 'Task Four' },
+    { ...TaskStories.Default.args.task, id: '5', title: 'Task Five' },
+    { ...TaskStories.Default.args.task, id: '6', title: 'Task Six' },
   ],
 };
 
