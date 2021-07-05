9ffb4f542563dda2efcd0c3786f01ed11c12fd2b
Aron Bachmann Árnason <aronba@islandsbanki.is>
2021-07-05 12:25:55 +0000

    Versioning test

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,12 @@
 module.exports = {
   stories: ["../src/app/components/**/*.stories.ts"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  ref: {
+    "chromatic-published-Storybook": {
+      // The title of your Storybook
+      title: "Design System",
+      // The url provided by Chromatic when it was published
+      url: "https://60df0940a562af0039d627ea-bmbihdppym.chromatic.com/",
+    },
+  },
 };
